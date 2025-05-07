f72c50147c8c29ef246f18b867c0ca351d97e8ba
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-07 19:56:15 -0300

    feat: Criando CÃlasse Service vazio

--- tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/TaskService.java
+++ tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/TaskService.java
@@ -20,29 +20,47 @@ public class TaskService {
     }
 
     public Object updateTask(UUID idUser, UUID idTask, RequestUpdateTaskDTO dto) {
+        // TODO: implement
+        return null;
     }
 
     public Object updateTasksAndTheirFutureRepetitions(UUID idUser, UUID idTask, RequestUpdateTaskDTO dto) {
+        // TODO: implement
+        return null;
     }
 
     public Object updateTaskStatus(UUID idUser, UUID idTask, String todo) {
+        // TODO: implement
+        return null;
     }
 
     public Object deleteTask(UUID idUser, UUID idTask) {
+        // TODO: implement
+        return null;
     }
 
     public Object deleteTasksAndTheirFutureRepetitions(UUID idUser, UUID idTask) {
+        // TODO: implement
+        return null;
     }
 
     public Object lockTaskByObjective(UUID idUser, String idObjective) {
+        // TODO: implement
+        return null;
     }
 
     public Object getTasksInDateRange(UUID idUser, Date startDate, Date endDate) {
+        // TODO: implement
+        return null;
     }
 
     public Object getTasksInDate(UUID idUser, Date date) {
+        // TODO: implement
+        return null;
     }
 
     public Object getTasksLate(UUID idUser) {
+        // TODO: implement
+        return null;
     }
 }
