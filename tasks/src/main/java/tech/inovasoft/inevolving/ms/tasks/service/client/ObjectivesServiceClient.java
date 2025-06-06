15499e53166ac8155f1fcced34eab87c2bf6f3c2
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-06 14:43:48 -0300

    Organizando codigo e TODOs

--- tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/client/ObjectivesServiceClient.java
+++ tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/client/ObjectivesServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-@FeignClient(name = "objectives-service", url = "http://localhost:8080/ms/objectives")
+@FeignClient(name = "objectives-service", url = "http://localhost:8080/ms/objectives") //TODO: Mudar para o endereço do container e esconder ele em variaveis de ambiente
 public interface ObjectivesServiceClient {
 
     @GetMapping("/{idObjective}/{idUser}")
