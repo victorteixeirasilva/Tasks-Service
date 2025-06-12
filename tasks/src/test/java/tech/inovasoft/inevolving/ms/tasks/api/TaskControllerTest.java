ac932cff4b9f992a28ae3a14fdde6712ce4fae9e
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-12 16:03:11 -0300

    Desenvolvendo teste de end-point, getTasksDoneInDate_ok

--- tasks/src/test/java/tech/inovasoft/inevolving/ms/tasks/api/TaskControllerTest.java
+++ tasks/src/test/java/tech/inovasoft/inevolving/ms/tasks/api/TaskControllerTest.java
@@ -708,7 +708,36 @@ public class TaskControllerTest {
 
     @Test
     public void getTasksDoneInDate_ok() {
-        //TODO: Desenvolver teste do End-Point
+        UUID idUser = UUID.randomUUID();
+
+        UUID idObjective = addObjective(idUser);
+        UUID idObjective2 = addObjective(idUser);
+
+        addTask(idObjective, idUser);
+        addTask(idObjective, idUser);
+        addTask(idObjective, idUser);
+        UUID id1 = addTask(idObjective2, idUser);
+        UUID id2 = addTask(idObjective2, idUser);
+        UUID id3 = addTask(idObjective2, idUser);
+        updateStatus(idUser, id1, Status.DONE);
+        updateStatus(idUser, id2, Status.DONE);
+        updateStatus(idUser, id3, Status.DONE);
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        String url = "http://localhost:"+port+"/ms/tasks/status/done/"+idUser+"/"+LocalDate.now();
+
+        ValidatableResponse response = requestSpecification
+                .when()
+                .get(url)
+                .then();
+
+        response.assertThat().statusCode(200);
+
+        List<Task> taskList = response.extract().body().jsonPath().get();
+
+        Assertions.assertEquals(3, taskList.size());
     }
 
     @Test
