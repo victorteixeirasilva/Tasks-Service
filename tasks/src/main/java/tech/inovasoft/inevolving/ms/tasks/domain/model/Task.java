990f08b63a5d19ca2e50579d986be9fc4c748bab
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-09 17:18:58 -0300

    Resolvendo conflito dos objetivos

--- tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/domain/model/Task.java
+++ tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/domain/model/Task.java
@@ -39,7 +39,7 @@ public class Task {
         this.descriptionTask = dto.descriptionTask();
         this.status = Status.TODO;
         this.dateTask = Date.valueOf(dto.dateTask());
-        this.setIdObjective(dto.idObjective());
+        this.idObjective = dto.idObjective();
         this.idUser = dto.idUser();
         this.isCopy = false;
     }
@@ -50,10 +50,7 @@ public class Task {
         this.status = Status.TODO;
         this.dateTask = Date.valueOf(dto.dateTask());
 
-        if (dto.idObjective() != null) {
-            this.setIdObjective(dto.idObjective());
-        }
-        this.setIdObjective(dto.idUser());
+        this.idObjective = dto.idObjective();
 
         this.idUser = dto.idUser();
         this.idOriginalTask = idOriginalTask;
