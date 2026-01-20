8b3b23edeb2ac5c1f5355235508f0b6274001557
victorteixeriasilva <victorteixeira2308@gmail.com>
2026-01-20 15:15:10 -0300

    Testando editar tarefas

--- tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/client/Auth_For_MService/AuthForMServiceClient.java
+++ tasks/src/main/java/tech/inovasoft/inevolving/ms/tasks/service/client/Auth_For_MService/AuthForMServiceClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import tech.inovasoft.inevolving.ms.tasks.service.client.Auth_For_MService.dto.AuthAuthenticationRequest;
 import tech.inovasoft.inevolving.ms.tasks.service.client.Auth_For_MService.dto.AuthLoginResponse;
 
-@FeignClient(name = "auth-service", url = "https://api.inevolving.inovasoft.tech/Auth-For-MService/auth/ms/authentication/login")
+//@FeignClient(name = "auth-service", url = "https://api.inevolving.inovasoft.tech/Auth-For-MService/auth/ms/authentication/login")
+@FeignClient(name = "auth-service", url = "http://auth-for-m-service:2723/auth/ms/authentication/login")
 public interface AuthForMServiceClient {
 
     @PostMapping("/{microServiceNameReceiver}")
